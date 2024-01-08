20ae9981db93c38f2f09269e566b25083070e3ba
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2024-01-08 09:44:32 +0100

    Astro: Start with fresh Portfolio template
    This updates to Astro 4, using the Portfolio starter.
    I want to first finish the portfolio portion of the site and then get
    back to adding the old blog posts and new article setup.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,38 +1,15 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
-function removeDupsAndLowerCase(array: string[]) {
-	if (!array.length) return array;
-	const lowercaseItems = array.map((str) => str.toLowerCase());
-	const distinctItems = new Set(lowercaseItems);
-	return Array.from(distinctItems);
-}
-
-const post = defineCollection({
-	type: "content",
-	schema: ({ image }) =>
-		z.object({
-			title: z.string().max(60),
-			description: z.string().min(50).max(160),
-			publishDate: z.string().transform((str) => new Date(str)),
-			coverImage: z
-				.object({
-					src: image(),
-					alt: z.string(),
-				})
-				.optional(),
-			tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
-			ogImage: z.string().optional(),
+export const collections = {
+	work: defineCollection({
+		type: 'content',
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
 		}),
-});
-
-const archivedPost = defineCollection({
-	type: "content",
-	schema: () =>
-		z.object({
-			name: z.string(),
-			title: z.string().max(120),
-			time: z.date(),
-		}),
-});
-
-export const collections = { post, archived_post: archivedPost };
+	}),
+};
